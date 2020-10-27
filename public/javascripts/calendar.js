b800c718899d71ce95ef9575ceffebeb5d305c15
AdrKacz <61620013+AdrKacz@users.noreply.github.com>
2020-10-27 21:33:22 +0100

    Calendar [0]
    Basic configuration of the calendar

---
+++ public/javascripts/calendar.js
@@ -0,0 +1,6 @@
+const calendarInput = document.querySelector('#calendar-input');
+
+calendarInput.addEventListener('click', () => {
+  // Design a static calendar in the page with modal bootstrap class
+  
+});
