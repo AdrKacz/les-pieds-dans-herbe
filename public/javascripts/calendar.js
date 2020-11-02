c140e7e0a7c6573843e4fca445ee078e8ae81b87
AdrKacz <61620013+AdrKacz@users.noreply.github.com>
2020-11-02 19:11:30 +0100

    Calendar [1]
    Set up for the layout, toggle button and start action

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -1,6 +1,73 @@
-const calendarInput = document.querySelector('#calendar-input');
+// MAIN const
 
-calendarInput.addEventListener('click', () => {
-  // Design a static calendar in the page with modal bootstrap class
-  
-});
+const MONTHS = [
+  {
+    long:'Janvier',
+    short:'Jan'
+  },
+  {
+    long:'Février',
+    short:'Fev'
+  },
+  {
+    long:'Mars',
+    short:'Mar'
+  },
+  {
+    long:'Avril',
+    short:'Jan'
+  },
+  {
+    long:'Mai',
+    short:'Jan'
+  },
+  {
+    long:'Juin',
+    short:'Jui'
+  },
+  {
+    long:'Juillet',
+    short:'Juil'
+  },
+  {
+    long:'Août',
+    short:'Aou'
+  },
+  {
+    long:'September',
+    short:'Sep'
+  },
+  {
+    long:'Octobre',
+    short:'Oct'
+  },
+  {
+    long:'Novembre',
+    short:'Nov'
+  },
+  {
+    long:'Décembre',
+    short:'Dec'
+  },
+]
+
+// const calendarInput = document.querySelector('#calendar-input');
+const calendarRoot = document.querySelector('#calendar');
+
+calendarRoot.onload = updateCalendar(MONTHS[10]);
+
+const startDateInput = '';
+const endDateInput = '';
+
+
+// calendarInput.addEventListener('click', () => {
+//   // Design a static calendar in the page with modal bootstrap class
+//   calendarRoot.modal('toggle');
+// });
+
+
+function updateCalendar(month) {
+  // Calendar Root
+  console.log(`Update Calendar [${month.long}]`);
+
+}
